8bbada4c2f098b3137fa58bcda53872aa291d126
Víctor Mardones <v.mardones04@ufromail.cl>
2021-09-07 10:10:52 -0300

    Antes del Tutorial 3

--- Taller 2/class.AmbienteConway.js
+++ Taller 2/class.AmbienteConway.js
@@ -160,6 +160,14 @@ class AmbienteConway {
                 }
             }
         }
+
+        if (nombrePatron == "boom") {
+            for (var i = 0; i < boom.length; i++) {
+                for (j of boom[i]) {
+                    this.activa(x+i, y+j);
+                }
+            }
+        }
     }
 }
 
@@ -184,4 +192,10 @@ const figuraOcho = [
     [1],
     [2, 4, 5],
     [4, 5]
+]
+
+const boom = [
+    [0, 1, 2, 4, 5, 6],
+    [0, 2, 4, 6],
+    [0, 1, 2, 4, 5, 6]
 ]
