86fb7ee48ce82e2eea10c0dd24832d725c75ac7c
Víctor Mardones <v.mardones04@ufromail.cl>
2021-09-21 11:30:18 -0300

    Tutorial 5 - Parte 5

--- Tutorial 5/index.php
+++ Tutorial 5/index.php
@@ -5,16 +5,17 @@
 <body>
 <?php
 
-$n = 10;
+$n = 6;
 $m = 8;
+$col = array("00", "35", "96", "B4", "CC", "F4");
+$col2 = array("0000", "3500", "9600", "9696", "96B4", "B4B4", "0096", "35F4");
 
-echo "<h2> tabla de $n x $m </h2>";
 echo "<table border='1'>";
 
 for ($i = 0; $i < $n; $i++) {
     echo "\n<tr>";
     for ($j = 0; $j < $m; $j++) {
-        echo "\n  <td> pos ($i, $j) </td>";
+        echo "\n  <td> ".$col[$i].$col2[$j]." </td>";
     }
     echo "\n</tr>";
 }
