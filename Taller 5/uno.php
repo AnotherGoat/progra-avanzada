db74125e698db1a9c64ef2933cd52c445db72514
Víctor Mardones <v.mardones04@ufromail.cl>
2021-09-21 12:20:08 -0300

    Taller 5 - Problema 1

---
+++ Taller 5/uno.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Primer formulario</title>
+</head>
+<body>
+<?php
+$nombres = $_GET["nombres"];
+$apellidos = $_GET["apellidos"];
+?>
+<h1> HOLA <?=$nombres?> <?=$apellidos?> </h1>
+</body>
+</html>
