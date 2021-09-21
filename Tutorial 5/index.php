a1c0f45a959e7e04c99a72520d0967277d9ba1a9
Víctor Mardones <v.mardones04@ufromail.cl>
2021-09-21 11:24:14 -0300

    Tutorial 5 - Parte 4

--- Tutorial 5/index.php
+++ Tutorial 5/index.php
@@ -5,10 +5,20 @@
 <body>
 <?php
 
-for($i = 6; $i > 1; $i--) {
-    echo "\n<h$i>HOLA</h$i>";
-}
+$n = 10;
+$m = 8;
 
+echo "<h2> tabla de $n x $m </h2>";
+echo "<table border='1'>";
+
+for ($i = 0; $i < $n; $i++) {
+    echo "\n<tr>";
+    for ($j = 0; $j < $m; $j++) {
+        echo "\n  <td> pos ($i, $j) </td>";
+    }
+    echo "\n</tr>";
+}
+echo "</table>";
 ?>
 
 </body>
