892d7d98fb347cc6b90bd0b94d1ae51c7559caa5
Víctor Mardones <v.mardones04@ufromail.cl>
2021-09-21 12:42:20 -0300

    Taller 5 - Problema 2

--- Taller 5/dos.php
+++ Taller 5/dos.php
@@ -2,13 +2,16 @@
 <html>
 <head>
     <meta charset="UTF-8">
-    <title>Primer Formulario</title>
+    <title>Segundo Formulario</title>
 </head>
 <body>
 <?php
 $nombres = $_GET["nombres"];
 $apellidos = $_GET["apellidos"];
+$direccion = $_GET["direccion"];
+$ciudad = $_GET["ciudad"];
 ?>
 <h1> HOLA <?=$nombres?> <?=$apellidos?> </h1>
+<h2> Ciudad: <?=$ciudad?>, Dirección: <?=$direccion?></h2>
 </body>
 </html>
