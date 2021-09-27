8860064e4b68ebc41f8c99327c4444cd547f0107
Víctor Mardones <v.mardones04@ufromail.cl>
2021-09-27 13:33:02 -0300

    Tutorial 6 - Parte 2

--- Tutorial 6/icc343clases.php
+++ Tutorial 6/icc343clases.php
@@ -2,7 +2,7 @@
 include_once "class.Ambiente.php";
 
 $amb = new Ambiente(1000, 800);
-$amb->generaEntesAlAzar(50, "#33AA44");
-$amb->generaEntesAlAzar(1, "#AA3344");
+$amb->generaEntesAlAzar(45, "#33AA44"); // verde
+$amb->generaEntesAlAzar(5, "#AA3344"); // rojo
 echo $amb->VistaSVG();
 ?>
